7d609f6919b710c14df414a9efc947a94b0e3cd7
Kenny <kennywcorman@gmail.com>
2013-11-03 10:21:05 -0800

    Started writing unit tests, got psql running locally (not technically part of this repo)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "at least one user exists" do
+    assert User.all.count > 0
+  end
+
+  test "user named eddy exists in db" do
+    assert User.all.pluck(:name).include?("eddy")
+  end
+
+  test "user named eddy has at least one work of art" do
+   
+    assert User.find_by_name("eddy").arts.count > 0, "Eddy should have at least one art"
+  end
 end
