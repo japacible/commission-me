87f2b9a2191af89a6b9eccd3a40a9b18aecf8626
japacible <japacible@gmail.com>
2013-11-21 00:45:33 -0800

    fix crash because of db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117223404) do
+ActiveRecord::Schema.define(version: 20131119215220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,24 +40,50 @@ ActiveRecord::Schema.define(version: 20131117223404) do
     t.text     "commission_request_data"
   end
 
-  create_table "fake_users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "conversations", force: true do |t|
+    t.string   "subject",    default: ""
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "messages", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "messages", force: true do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.boolean  "sender_deleted",    default: false
-    t.boolean  "recipient_deleted", default: false
-    t.string   "subject"
+  create_table "notifications", force: true do |t|
+    t.string   "type"
     t.text     "body"
-    t.datetime "read_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "subject",              default: ""
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                default: false
+    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                           null: false
+    t.integer  "notified_object_id"
+    t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.string   "attachment"
+    t.boolean  "global",               default: false
+    t.datetime "expires"
   end
+
+  add_index "notifications", ["conversation_id"], name: "index_notifications_on_conversation_id", using: :btree
+
+  create_table "receipts", force: true do |t|
+    t.integer  "receiver_id"
+    t.string   "receiver_type"
+    t.integer  "notification_id",                            null: false
+    t.boolean  "is_read",                    default: false
+    t.boolean  "trashed",                    default: false
+    t.boolean  "deleted",                    default: false
+    t.string   "mailbox_type",    limit: 25
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
+
+  add_index "receipts", ["notification_id"], name: "index_receipts_on_notification_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
