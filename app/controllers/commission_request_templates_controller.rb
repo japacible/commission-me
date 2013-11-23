c95d60f202fe42ae071e97f65a5a070599bad5dc
Kenny <kennywcorman@gmail.com>
2013-11-23 15:51:06 -0800

    Not a fully functional commit, but saving changes. Created controllers, moved static files.

---
+++ app/controllers/commission_request_templates_controller.rb
@@ -0,0 +1,21 @@
+#this controller exists for controlling the blob of stuff that represents
+#an artist's commission request template
+class CommissionRequestTemplatesController < ApplicationController
+  #Should eventually be removed (all artists start with a 
+  #default template)
+  def new
+    
+  end
+  
+  #Renders a view
+  def edit
+
+  end
+
+  #Receives post data
+  def update
+
+  end
+
+
+end
