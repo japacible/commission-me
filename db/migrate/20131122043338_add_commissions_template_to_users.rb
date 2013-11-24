2954914ecdad5c5f0314d116af6115fe65adaad0
japacible <japacible@gmail.com>
2013-11-23 21:48:27 -0800

    rm line to fix db migrate issues in users

--- db/migrate/20131122043338_add_commissions_template_to_users.rb
+++ db/migrate/20131122043338_add_commissions_template_to_users.rb
@@ -1,6 +1,5 @@
 class AddCommissionsTemplateToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :commission_request_template
     add_column :users, :commission_request_template_json, :json
   end
 end
