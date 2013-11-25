5d650480c1371fb01f8e55e0f5c6e8f58697ece9
Kenny <kennywcorman@gmail.com>
2013-11-24 19:53:57 -0800

    Fixed commission not saving bug by removing attr_accessor. Supposedly it was shadowing the ActiveRecord attributes or something (but I swear it didn't used to O.o)

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,5 +1,5 @@
 class Commission < ActiveRecord::Base
-  attr_accessor :state, :commissioner_id, :artist_id, :commission_current
+  # attr_accessor :state, :commissioner_id, :artist_id, :commission_current
   belongs_to :artist, :class_name => User
   belongs_to :commissioner, :class_name => User
   has_one :commission_request, :class_name => CommissionRequest, :foreign_key =>
