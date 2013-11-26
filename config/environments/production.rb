518b49f67b9eb554582c7add70fac49e7f4fe462
japacible <japacible@gmail.com>
2013-11-25 20:07:28 -0800

    add lines to try to make heroku precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,12 @@ Myapp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.initialize_on_precompile = false 
+
+  config.assets.precompile += %w( comreq.js )
+  config.assets.precompile += %w( comrev.js )
+  config.assets.precompile += %w( comset.js )
+  config.assets.precompile += %w( reqlist.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
