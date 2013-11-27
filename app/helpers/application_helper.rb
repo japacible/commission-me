b215acc8472534040f6030f44cf4d0d5c78a7651
cquan <christinaquan@Christinas-MacBook-Pro-2.local>
2013-11-27 00:54:14 -0800

    Update alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
 		return @title if defined?(@title)
 		"CommissionMe"
 	end
+    
+    def flash_class(level)
+        case level
+            when :notice then "alert alert-info"
+            when :success then "alert alert-success"
+            when :error then "alert alert-error"
+            when :alert then "alert alert-error"
+        end
+    end
+    
 end
